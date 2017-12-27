5388997b060508db0c20cd5d12e0645b755c7d31
Shun Onda <username@example.com>
2017-12-27 22:46:38 +0900

    First Commit

---
+++ .emacs.d/conf/10_display.el
@@ -0,0 +1,15 @@
+;; -----------------------
+;; モードラインの設定
+;; -----------------------
+;; 改行コードを表示する
+(setq eol-mnemonic-dos "(CRLF)")
+(setq eol-mnemonic-mac "(CR)")
+(setq eol-mnemonic-unix "(LF)")
+;; メニューバーを消す
+(menu-bar-mode -1)
+;; ツールバーを消す
+(tool-bar-mode -1)
+;; 列数を表示する
+(column-number-mode t)
+;; 行数を表示する
+;;(global-linum-mode t)
